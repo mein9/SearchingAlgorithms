1273e915d364253ae282226304ac0f83f4cdc34f
mein9 <mateo0221@gmail.com>
2024-07-09 14:05:09 -0700

    working search

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr1 = { 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int arr1Result = LinearSearch(arr1, 4);
+        }
+
+        static int LinearSearch(int[] arrToSearch, int target)
+        {
+            //intitialize loop at 0
+            //next we evaluate our expression
+            //last we run the update logic
+            for (int i = 0; i < arrToSearch.Length; i++)
+            {
+                if (arrToSearch[i] == target) return i;
+            }
+
+            return -1;
         }
     }
 }
